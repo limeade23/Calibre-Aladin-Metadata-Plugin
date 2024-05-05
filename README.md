8c09a563ae61009fc943342dae8291415268bd4b
limeade23 <junhan.koo@limeade.me>
2024-05-06 06:10:00 +0900

    알라딘 플러그인 커밋

---
+++ README.md
@@ -0,0 +1,15 @@
+# Calibre Aladin OpenAPI Metadata Source Plugin
+
+Aladin OpenAPI를 이용한 캘리버 메타데이터 다운로드 플러그인
+
+## 설치 방법
+1. [알라딘 OpenAPI](https://www.aladin.co.kr/ttb/wblog_manage.aspx)에서 API를 발급
+2. `__init__.py` 파일을 열고 `API_KEY`에는 Open API 인증키를 입력한다.
+3. `__init__.py` 파일을 압축 후 Calibre에서 플러그인 등록한다.
+4. 메타데이터 편집하기에서 메타데이터 다운로드를 클릭하면 책 정보와 책 표지를 받아오게 된다.
+
+### 참고사항
+- calibre 7.10에서 테스트되었습니다.
+- 언어는 한국어로 고정되어 있습니다.
+- 알라딘 카테고리가 태그로 분류됩니다.
+- 사용자 리뷰 평점이 반영됩니다.
---
+++ __init__.py
@@ -0,0 +1,166 @@
+from typing import Optional
+from datetime import datetime
+from urllib.parse import urlencode
+from urllib.request import urlopen, Request
+import json
+import re
+
+from calibre.ebooks.metadata.book.base import Metadata
+from calibre.ebooks.metadata.sources.base import Source
+from PyQt5.Qt import QWidget
+from calibre.gui2.store.basic_config_widget_ui import Ui_Form
+
+
+class Aladin(Source):
+
+    name = "Aladin OpenAPI"
+    description = "Downloads metadata and covers from Aladin Open API."
+    author = "Limeade23 <https://github.com/limeade23>"
+    version = (0, 0, 1)
+    minimum_calibre_version = (6, 10, 0)
+
+    ALADIN_ID: str = "aladin"
+    API_KEY: str = ""
+    API_URL: str = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx"
+
+    capabilities = frozenset(["identify", "cover"])
+    touched_fields = frozenset(
+        [
+            "title",
+            "authors",
+            "identifier:" + ALADIN_ID,
+            "identifier:isbn",
+            "comments",
+            "publisher",
+            "pubdate",
+            "languages",
+            "tags",
+            "rating",
+        ]
+    )
+
+    def identify(
+        self,
+        log,
+        result_queue,
+        abort,
+        title=None,
+        authors=None,
+        identifiers={},
+        timeout=30,
+    ):
+        search_keyword = title
+        if title and authors:
+            authors_str = ",".join(authors)
+            search_keyword = f"{title} {authors_str}"
+        books = self._search(search_keyword)
+        for book in books:
+            metadata = self._to_metadata(book)
+            if isinstance(metadata, Metadata):
+                mi = metadata.identifiers[self.ALADIN_ID]
+                if metadata.cover_url:
+                    self.cache_identifier_to_cover_url(mi, metadata.cover_url)
+                self.clean_downloaded_metadata(metadata)
+                result_queue.put(metadata)
+
+    def _search(self, title: str = "", timeout: int = 30):
+        params = {
+            "ttbkey": self.API_KEY,
+            "Query": title,
+            "QueryType": "Keyword",
+            "MaxResults": 10,
+            "start": 1,
+            "SearchTarget": "All",
+            "output": "js",
+            "Version": 20131101,
+            "OptResult": "subInfo",
+            "Cover": "Big",
+        }
+
+        query_string = urlencode(params)
+        url = f"{self.API_URL}?{query_string}"
+
+        request = Request(
+            url,
+            method="GET",
+            headers={"Accept": "application/json", "Content-Type": "application/json"},
+        )
+
+        with urlopen(request, timeout=timeout) as response:
+            body = response.read()
+            if response.status != 200:
+                raise Exception(f"failed to search: {body.decode()}")
+
+        results = json.loads(body).get("item", [])
+
+        if len(results) > 0:
+            return results
+
+        return None
+
+    def _to_metadata(self, data: dict) -> Metadata:
+        authors = data.get("author", "").split(",")
+
+        metadata = Metadata(data.get("title", ""), authors)
+        metadata.comments = data.get("description", "")
+        identifiers = data.get("itemId")
+        metadata.set_identifier(self.ALADIN_ID, str(identifiers))
+        metadata.isbn = data.get("isbn13", "")
+        metadata.tags = data.get("categoryName", "").split(">")
+        metadata.rating = data.get("customerReviewRank")
+
+        date_string = data.get("pubDate")
+        formatted_date = datetime.strptime(date_string, "%Y-%m-%d")
+        metadata.pubdate = formatted_date
+        metadata.publisher = data.get("publisher", "")
+        metadata.language = "kor"
+        metadata.cover_url = data.get("cover", "")
+
+        return metadata
+
+    def get_cached_cover_url(self, identifiers):
+
+        url = None
+        mi = identifiers.get(self.ALADIN_ID, None)
+        if mi is not None:
+            url = self.cached_identifier_to_cover_url(mi)
+        return url
+
+    def download_cover(
+        self,
+        log,
+        result_queue,
+        abort,
+        title=None,
+        authors=None,
+        identifiers={},
+        timeout=30,
+        get_best_cover=False,
+    ):
+        cover_url = self.get_cached_cover_url(identifiers)
+        if cover_url:
+            
+            log.info(
+                    "Trying to download cover from: %s",
+                    cover_url.replace("cover200", "cover500"),
+                )
+            try:
+                # Attempt to download cover from cover200 URL
+                with urlopen(
+                    cover_url.replace("cover200", "cover500"), timeout=timeout
+                ) as response:
+                    cover = response.read()
+                    result_queue.put((self, cover))
+
+            except Exception as e:
+                log.exception("Failed to download cover from: %s", cover_url)
+                log.info("Downloading cover from: %s", cover_url)
+                # If there is an error, try downloading from cover500 URL
+                try:
+                    with urlopen(cover_url, timeout=timeout) as response:
+                        cover = response.read()
+                        result_queue.put((self, cover))
+                except Exception as e:
+                    log.info("Downloading cover from: %s", cover_url)
+        else:
+            log.info("No cover found")
